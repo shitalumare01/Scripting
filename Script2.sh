4acc9d444b5fa4938b4758603e3022e64972d893
Shital Sunil Umare <137820080+shitalumare01@users.noreply.github.com>
2025-11-12 09:39:54 +0530

    Update Script2.sh

--- Script2.sh
+++ Script2.sh
@@ -4,6 +4,4 @@ echo "Value of a"
 read a
 echo "Value of b"
 read b
-echo "Hello value of a is $a and value of b is $b"
-echo "My name is Shital"
-echo "My surname is Umare"
+echo "Hello value of a is $a and value of b is $
